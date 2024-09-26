bbe246cd996905427c0b9ac462b583e63b437700
Per Stensland <per.stensland@oneagency.se>
2024-09-26 13:08:02 +0200

    add migration for different database

---
+++ api/Migrations/20240926105948_functional_v0_1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyTravels.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class functional_v0_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Media");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Media",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Media",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Name",
+                value: "IMG-20210215-WA0017");
+
+            migrationBuilder.UpdateData(
+                table: "Media",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Name",
+                value: "Dsc344");
+        }
+    }
+}
